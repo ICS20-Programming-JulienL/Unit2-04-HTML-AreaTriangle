d9483d43fc616229d90cb74b2eb71e5c4547a876
Lamour123 <124379380+Lamour123@users.noreply.github.com>
2023-03-20 22:06:31 +0000

    I finished the layout of the webpage.

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2023 Julien Lamoureux inc. All rights reserved
+//
+// Created by: Julien Lamoureux
+// Created on: Mar 2023
+// This file contains the JS functions for index.html
+
+"use strict";
+
+function enterClicked() {
+// get the base of the triangle
+  let base= parseFloat(document.getElementById("base").value)
+	// get the height of the triangle
+  let height= parseFloat(document.getElementById("height").value)
+
+
+  // display the area to the user
+  document.getElementById('Answer').innerHTML = "The area of the triangle is " +height*base*0.5+"."
+}
